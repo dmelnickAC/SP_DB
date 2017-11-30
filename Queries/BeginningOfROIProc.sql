842a44f5ffd96c0f73147cf1ba7c8a9989c5e1fa
dmelnickAC <32101444+dmelnickAC@users.noreply.github.com>
2017-11-30 15:02:05 -0600

    foo

--- Queries/BeginningOfROIProc.sql
+++ Queries/BeginningOfROIProc.sql
@@ -1,8 +1,17 @@
+
+
 SELECT DISTINCT
-	client_id
-	, provider_creating_id
-	, provider_id
-FROM chicago_export.dbo.sp_release_of_info
+	roi.client_id
+	, roi.provider_creating_id
+	, roi.provider_id
+	, MAX(u.email) AS Email
+FROM chicago_export.dbo.sp_release_of_info roi
+INNER JOIN chicago_export.dbo.sp_user u
+	ON u.provider_creating_id = roi.provider_creating_id
 WHERE
 	documentation like 'B - %'
 	or documentation like 'D - %'
+GROUP BY
+		roi.client_id
+	, roi.provider_creating_id
+	, roi.provider_id
